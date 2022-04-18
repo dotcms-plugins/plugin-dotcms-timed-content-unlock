5181711d29d4c99ca0ffe1230431eea7537120ec
Will Ezell <will@dotcms.com>
2022-04-18 17:26:03 -0400

    working on 22.03

--- src/main/java/com/dotcms/job/Activator.java
+++ src/main/java/com/dotcms/job/Activator.java
@@ -1,34 +1,22 @@
 package com.dotcms.job;
 
-import com.dotmarketing.loggers.Log4jUtil;
-import com.dotmarketing.osgi.GenericBundleActivator;
-import com.dotmarketing.quartz.CronScheduledTask;
-import com.dotmarketing.servlets.InitServlet;
-import com.dotmarketing.util.Logger;
-import com.google.common.collect.ImmutableList;
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.dynamic.ClassFileLocator;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.osgi.framework.BundleContext;
-import org.quartz.CronTrigger;
-
-import java.util.Calendar;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static com.dotcms.job.UnlockContentTimer.SQL_LIMIT_CLAUSE;
-import static com.dotcms.job.UnlockContentTimer.THREAD_SLEEP_BETWEEN_UNLOCKS;
-import static com.dotcms.job.UnlockContentTimer.UNLOCK_AFTER_SECONDS;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.core.util.CronExpression;
+import org.osgi.framework.BundleContext;
+import com.dotmarketing.osgi.GenericBundleActivator;
+import com.dotmarketing.util.Logger;
 
 /**
- * This OSGi Plugin will automatically unlock content that has been locked for X amount of time. You can specify the
- * duration to wait before unlocking by modifying the {@code UNLOCK_AFTER_SECONDS} property in the {@code
- * plugin.properties} file found under the {@code src/main/resources/} directory. You can also modify the {@code
+ * This OSGi Plugin will automatically unlock content that has been locked for X amount of time. You
+ * can specify the duration to wait before unlocking by modifying the {@code UNLOCK_AFTER_SECONDS}
+ * property in the {@code
+ * plugin.properties} file found under the {@code src/main/resources/} directory. You can also
+ * modify the {@code
  * CRON_EXPRESSION} expression of the cron job for the job itself.
  *
  * @author dotCMS
@@ -40,15 +28,15 @@ public class Activator extends GenericBundleActivator {
     public final static String JOB_GROUP = "OSGi Jobs";
     public final static String JOB_CLASS = "com.dotcms.job.UnlockContentTimer";
 
-    private final List<Class> overriddenClasses = ImmutableList.of(UnlockContentTimer.class);
-    private final ClassLoader webappClassLoader = InitServlet.class.getClassLoader();
-    private final ClassLoader bundleClassLoader = this.getClass().getClassLoader();
-    private final ClassReloadingStrategy classReloadingStrategy = ClassReloadingStrategy.fromInstalledAgent();
-    private LoggerContext pluginLoggerContext;
+
+    private final Runnable runner = new UnlockContentTimer();
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
 
     /**
-     * This is the staring point for all OSGi plugins in dotCMS. This is where all the setup and basic configuration
-     * routines must be called.
+     * This is the staring point for all OSGi plugins in dotCMS. This is where all the setup and basic
+     * configuration routines must be called.
      *
      * @param context The OSGi {@link BundleContext} object.
      *
@@ -56,34 +44,28 @@ public class Activator extends GenericBundleActivator {
      */
     @Override
     public void start(final BundleContext context) throws Exception {
-        // Initializing log4j
-        final LoggerContext dotcmsLoggerContext = Log4jUtil.getLoggerContext();
-        // Initialing the log4j context of this plugin based on the dotCMS logger context
-        this.pluginLoggerContext = (LoggerContext) LogManager.getContext(this.getClass().getClassLoader(), false,
-                dotcmsLoggerContext, dotcmsLoggerContext.getConfigLocation());
-        // Initializing services
-        initializeServices(context);
-        injectClassesIntoDotCMS();
-        final Map<String, Object> jobParams = new HashMap<>();
-        jobParams.put(UNLOCK_AFTER_SECONDS, OSGiPluginProperties.getProperty(UNLOCK_AFTER_SECONDS, "86400"));
-        jobParams.put(SQL_LIMIT_CLAUSE, OSGiPluginProperties.getProperty(SQL_LIMIT_CLAUSE, "1000"));
-        jobParams.put(THREAD_SLEEP_BETWEEN_UNLOCKS, OSGiPluginProperties.getProperty(THREAD_SLEEP_BETWEEN_UNLOCKS,
-                "50"));
-        final String cronExpression = OSGiPluginProperties.getProperty("CRON_EXPRESSION");
-        // Wait until the next minute before firing the first time
-        final Calendar startTime = Calendar.getInstance();
-        startTime.add(Calendar.SECOND, 0);
-        // Creating the custom Quartz Job
-        final CronScheduledTask cronScheduledTask = new CronScheduledTask(JOB_NAME, JOB_GROUP, JOB_NAME, JOB_CLASS,
-                startTime.getTime(), null, CronTrigger.MISFIRE_INSTRUCTION_FIRE_ONCE_NOW, jobParams, cronExpression);
-        Logger.info(this.getClass().getName(), "Adding UnlockContentTimer Job to Quartz");
-        // Schedule the custom job
-        scheduleQuartzJob(cronScheduledTask);
+
+        CronExpression cron = new CronExpression(OSGiPluginProperties.getProperty("CRON_EXPRESSION", "0 15 * * * ?")) ;
+        
+        Instant now = Instant.now();
+        
+        Instant previousRun = cron.getPrevFireTime(Date.from(now)).toInstant();
+        Instant nextRun = cron.getNextValidTimeAfter(Date.from(now)).toInstant();
+
+        long delay = Duration.between(previousRun, now).getSeconds();
+        long runEvery = Duration.between(previousRun, nextRun).getSeconds();
+        
+        Logger.info(this.getClass(), "Starting Content Unlock Job. Runs every:" + runEvery + " seconds. Next run @ " + nextRun);
+        
+        scheduler.scheduleAtFixedRate(runner, delay, runEvery, TimeUnit.SECONDS);
+
+
+
     }
 
     /**
-     * Allows developers to correctly stop/un-register/remove Services and other utilities when an OSGi Plugin is
-     * stopped.
+     * Allows developers to correctly stop/un-register/remove Services and other utilities when an OSGi
+     * Plugin is stopped.
      *
      * @param context The OSGi {@link BundleContext} object.
      *
@@ -91,45 +73,11 @@ public class Activator extends GenericBundleActivator {
      */
     @Override
     public void stop(final BundleContext context) throws Exception {
-        removeClassesFromDotCMS();
-        // Unregister all the bundle services
-        unregisterServices(context);
-        // Shutting down log4j in order to avoid memory leaks
-        Log4jUtil.shutdown(this.pluginLoggerContext);
-    }
-
-    /**
-     * Injects one or more classes used by this plugin into the dotCMS context. That is, the classes in this bundle are
-     * added to the dotCMS Class Loader.
-     */
-    protected void injectClassesIntoDotCMS() {
-        for (final Class clazz : this.overriddenClasses) {
-            Logger.info(this.getClass().getName(), String.format("Injecting class '%s' into classloader '%s'", clazz
-                            .getName(), this.webappClassLoader));
-            Logger.debug(this.getClass().getName(), "Bundle classloader: " + this.bundleClassLoader);
-            Logger.debug(this.getClass().getName(), "dotCMS classloader: " + this.webappClassLoader);
-            ByteBuddyAgent.install();
-            new ByteBuddy().rebase(clazz, ClassFileLocator.ForClassLoader.of(this.bundleClassLoader)).name(clazz
-                    .getName()).make().load(this.webappClassLoader, this.classReloadingStrategy);
-        }
+        
+        Logger.info(this.getClass(), "Stopping Content Unlock Job");
+        scheduler.shutdownNow();
     }
 
 
-    /**
-     * Removes one or more classes used by this plugin from the dotCMS context. That is, the classes in this bundle are
-     * removed from the dotCMS Class Loader.
-     */
-    protected void removeClassesFromDotCMS() throws Exception {
-        for (final Class clazz : this.overriddenClasses) {
-            Logger.info(this.getClass().getName(), String.format("Removing class '%s' from classloader '%s'", clazz
-                    .getName(), this.webappClassLoader));
-            try {
-                this.classReloadingStrategy.reset(ClassFileLocator.ForClassLoader.of(this.webappClassLoader), clazz);
-            } catch (final Exception e) {
-                Logger.debug(this.getClass().getName(),
-                        String.format("Error setting class '%s' back into dotCMS classloader", clazz.getName()));
-            }
-        }
-    }
 
 }
--- src/main/java/com/dotcms/job/UnlockContentTimer.java
+++ src/main/java/com/dotcms/job/UnlockContentTimer.java
@@ -1,5 +1,6 @@
 package com.dotcms.job;
 
+import com.dotcms.business.CloseDB;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.db.DbConnectionFactory;
@@ -10,6 +11,7 @@ import com.dotmarketing.util.Logger;
 import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.model.User;
 import com.liferay.util.StringPool;
+import io.vavr.control.Try;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -20,90 +22,118 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * This Quartz Job takes care of unlocking Contentlets that have been locked for X amount of time. All the configuration
- * properties are passed down to this class by the {@link OSGiPluginProperties} utility class. This is what the Job does
- * in order to unlock the appropriate Contentlets:
+ * This Job takes care of unlocking Contentlets that have been locked for X amount of time.
+ * All the configuration properties are passed down to this class by the
+ * {@link OSGiPluginProperties} utility class. This is what the Job does in order to unlock the
+ * appropriate Contentlets:
  * <ul>
- *     <li>Executes a raw SQL query that will return the Contentlet working Inodes who have been locked by the time
- *     indicated in the {@code UNLOCK_AFTER_SECONDS} property. The default value is {@code 86400} seconds, that is,
- *     1 day.</li>
- *     <li>Based on such an Inode, Contentlet is both retrieved and unlocked via the Content API.</li>
- *     <li>Just as a safety precaution, the Job will sleep for the number of milliseconds specified via the
- *     {@code THREAD_SLEEP_BETWEEN_UNLOCKS} property in order to let dotCMS appropriately update the Contenlet. The
- *     default value is {@code 50} milliseconds.</li>
- *     <li>The unlock process is carried out in batches whose size is specified by the {@code SQL_LIMIT_CLAUSE}
- *     property. The default batch size is {@code 1000} items.</li>
+ * <li>Executes a raw SQL query that will return the Contentlet working Inodes who have been locked
+ * by the time indicated in the {@code UNLOCK_AFTER_SECONDS} property. The default value is
+ * {@code 86400} seconds, that is, 1 day.</li>
+ * <li>Based on such an Inode, Contentlet is both retrieved and unlocked via the Content API.</li>
+ * <li>Just as a safety precaution, the Job will sleep for the number of milliseconds specified via
+ * the {@code THREAD_SLEEP_BETWEEN_UNLOCKS} property in order to let dotCMS appropriately update the
+ * Contenlet. The default value is {@code 50} milliseconds.</li>
+ * <li>The unlock process is carried out in batches whose size is specified by the
+ * {@code SQL_LIMIT_CLAUSE} property. The default batch size is {@code 1000} items.</li>
  * </ul>
- * By default, the Unlock Content Job will look for 1,000 Contentlets up to 1,000 times. This means that the Job unlocks
- * up to 1,000,0000 Contentlets in every execution. Therefore, if more than 1 million Contentlets are locked, the rest
- * of them will need to wait for the next time the Job runs.
+ * By default, the Unlock Content Job will look for 1,000 Contentlets up to 1,000 times. This means
+ * that the Job unlocks up to 1,000,0000 Contentlets in every execution. Therefore, if more than 1
+ * million Contentlets are locked, the rest of them will need to wait for the next time the Job
+ * runs.
  *
  * @author dotCMS
  * @since Aug 10, 2021
  */
-public class UnlockContentTimer implements StatefulJob {
+public class UnlockContentTimer implements Runnable {
 
     public static final String UNLOCK_AFTER_SECONDS = "UNLOCK_AFTER_SECONDS";
     public static final String SQL_LIMIT_CLAUSE = "SQL_LIMIT_CLAUSE";
     public static final String THREAD_SLEEP_BETWEEN_UNLOCKS = "THREAD_SLEEP_BETWEEN_UNLOCKS";
 
-    private static final boolean DONT_RESPECT_FRONTEND_ROLES = Boolean.FALSE;
 
+    final int unlockAfterSeconds =
+                    Try.of(() -> Integer.parseInt(OSGiPluginProperties.getProperty("UNLOCK_AFTER_SECONDS"))).getOrElse(86400);
+    final int sqlLimit = Try.of(() -> Integer.parseInt(OSGiPluginProperties.getProperty("SQL_LIMIT_CLAUSE"))).getOrElse(1000);
+    final int threadSleepBetweenLocks = Try
+                    .of(() -> Integer.parseInt(OSGiPluginProperties.getProperty("THREAD_SLEEP_BETWEEN_UNLOCKS"))).getOrElse(200);
+
+    @CloseDB
     @Override
-    public void execute(final JobExecutionContext context) throws JobExecutionException {
-        Logger.info(this, "dotCMS Timed Unlock Job: ------------------------------------------");
-        final JobDataMap dataMap = context.getJobDetail().getJobDataMap();
-        final String unlockAfter = dataMap.getString(UNLOCK_AFTER_SECONDS).toString();
+    public void run() {
+
+        
+        Logger.info(this.getClass(), "UnlockContentTimer : ------------------------------------------");
+        final String oldestServer = Try.of(() -> APILocator.getServerAPI().getOldestServer()).getOrElse("notIt");
+        if (!oldestServer.equals(APILocator.getServerAPI().readServerId())) {
+            
+            Logger.info(this.getClass(), "Not the oldest server in cluster, skipping UnlockContentTimer");
+            return;
+        }
+
+
+        Logger.info(this.getClass(), "UnlockContentTimer : ------------------------------------------");
+
         final Calendar lockedDate = Calendar.getInstance();
-        final int seconds = Integer.parseInt(unlockAfter);
-        lockedDate.add(Calendar.SECOND, -seconds);
+
+        lockedDate.add(Calendar.SECOND, -unlockAfterSeconds);
         String contentletInode = StringPool.BLANK;
         try {
             final DotConnect db = new DotConnect();
-            final User systemUser = APILocator.getUserAPI().getSystemUser();
-            final int sqlLimitClause = Integer.parseInt(dataMap.getString(SQL_LIMIT_CLAUSE).toString());
-            final long threadSleep = Integer.parseInt(dataMap.getString(THREAD_SLEEP_BETWEEN_UNLOCKS).toString());
             int unlockedContentlets = 0;
             for (int batch = 1; batch <= 1000; batch++) {
-                db.setSQL("SELECT working_inode FROM contentlet_version_info WHERE locked_on < ? AND locked_by IS NOT" +
-                        " NULL");
-                db.setMaxRows(sqlLimitClause);
+                db.setSQL("SELECT working_inode FROM contentlet_version_info WHERE locked_on < ? AND locked_by IS NOT NULL");
+                db.setMaxRows(sqlLimit);
                 db.addParam(lockedDate.getTime());
                 final List<Map<String, Object>> contentletList = db.loadObjectResults();
-                if (contentletList.isEmpty()) {
+                DbConnectionFactory.closeSilently();
+                if(contentletList.isEmpty()) {
                     break;
                 }
+                Logger.info(this.getClass(), "Run # : " + batch);
+                Logger.info(this.getClass(), "Found : " + contentletList.size() + " contentlets to unlock.");
+                
+
+                
+                
                 for (final Map<String, Object> contentletData : contentletList) {
-                    if (!UtilMethods.isSet(contentletData.get("working_inode"))) {
-                        continue;
+                    contentletInode = (String) contentletData.get("working_inode");
+                    if (unlockContentlet(contentletInode)) {
+                        unlockedContentlets++;
                     }
-                    contentletInode = contentletData.get("working_inode").toString();
-                    final Contentlet contentlet = APILocator.getContentletAPI().find(contentletInode, systemUser,
-                            DONT_RESPECT_FRONTEND_ROLES);
-                    APILocator.getContentletAPI().unlock(contentlet, systemUser, DONT_RESPECT_FRONTEND_ROLES);
-                    unlockedContentlets++;
-                    Thread.sleep(threadSleep);
+                    Thread.sleep(threadSleepBetweenLocks);
+
                 }
-                Logger.info(this.getClass(), "dotCMS Timed Unlock Job: Run #" + batch);
+                
             }
             Logger.info(this.getClass(), "dotCMS Timed Unlock Job: Unlocked " + unlockedContentlets + " contentlets");
-        } catch (final DotDataException e) {
-            Logger.error(this.getClass(), String.format("An error occurred when updating data in Contentlet '%s': " +
-                    "%s", contentletInode, e.getMessage()), e);
-        } catch (final DotSecurityException e) {
-            Logger.error(this.getClass(), String.format("An permissions error occurred when updating data in " +
-                    "Contentlet '%s': %s", contentletInode, e.getMessage()), e);
-        } catch (final InterruptedException e) {
-            Logger.error(this.getClass(), String.format("An error occurred during the Thread Sleep phase when " +
-                    "updating data in Contentlet '%s': %s", contentletInode, e.getMessage()), e);
+        } catch (final Exception e) {
+            Logger.error(this.getClass(),
+                            String.format("An error occurred unlocking content '%s': %s", contentletInode, e.getMessage()), e);
         } finally {
-            try {
-                DbConnectionFactory.closeSilently();
-            } catch (final Exception e) {
-                Logger.error(this.getClass(), String.format("dotCMS Timed Unlock Job: DB connection could not be " +
-                        "closed: %s", e.getMessage()), e);
-            }
+            DbConnectionFactory.closeSilently();
+
         }
     }
+
+    @CloseDB
+    final boolean unlockContentlet(final String inode) {
+        if (inode == null) {
+            return false;
+        }
+        final User systemUser = APILocator.systemUser();
+        try {
+            APILocator.getContentletAPI().unlock(APILocator.getContentletAPI().find(inode, systemUser, false), systemUser, false);
+        } catch (Exception e) {
+            Logger.warn(this.getClass(),
+                            String.format("An error occurred unlocking content inode: '%s': %s", inode, e.getMessage()), e);
+            return false;
+        }
+        return true;
+
+
+    }
+
+
 
 }
